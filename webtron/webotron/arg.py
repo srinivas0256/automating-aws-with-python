5c0e11539750ad832e5d44e35d2c27af884e3afd
EC2 Default User <ec2-user@ip-172-31-38-128.ec2.internal>
2019-07-05 11:39:40 +0000

    all the commits

--- webtron/webotron/arg.py
+++ webtron/webotron/arg.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python3.7
 import boto3
-import sys
+import click
 
 session = boto3.Session(profile_name='pythonautomation')
 s3 = session.resource('s3')
 
-if __name__ == '__main__':
-    print(sys.argv)
+@click.group()
+def cli():
+    "Webotron deploys websites to AWS"
+    pass
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
+
+@cli.command('list-bucket-objects')
+def list_bucket_objects():
+    "List objects in an s3 bucket"
+    for obj in s3.Bucket('atomatingawssrini-boto3').objects.all():
+        print(obj)
+
+if __name__ == '__main__':
+    cli()
